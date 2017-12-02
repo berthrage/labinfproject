f91deead572faeff6f543da122e3ef734ee2a3be
Nicole Beatriz <33996960+NihBeatriz@users.noreply.github.com>
2017-12-02 20:47:43 -0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+#include "menu.h"
+#include "funcoes1.h"
+#include "main.h"
+#include "listarAgenda.h"
+#include "ordemAlfTotal.h"
+#include "ordemAlf.h"
+#include "ordemLocal.h"
+#include "excluirContato.h"
+
+int main(){
+	int auxnulo;
+
+	// Permite usar acentuação
+    setlocale(LC_ALL, "Portuguese");
+
+    // Mudar cor da fonte
+    system("color B");
+
+	//Classificar todo o vetor das pessoas da agenda como vazio
+	for (auxnulo = 2; auxnulo < TAMAGENDA; auxnulo++){
+		strcpy(pessoa[auxnulo].telefone.numero, "NULL");
+	}
+
+    menu();
+
+	getch();
+	return 0;
+}
